f83a4a30bbaa19913c9623a5c4173bd276a070bd
nishikura <n-ishikura@tss-ipnet.co.jp>
2017-04-13 17:42:14 +0900

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,10 @@
-module ApplicationHelper
+require 'test_helper'
 
-# ページごとの完全なタイトルを返す
-  def full_title(page_title = '')	  # メソッド定義引数
-	base_title = "Ruby on Rails Tutorial Sample App"  # 変数の代入とオプション
-	if page_title.empty?	# 論理値でテスト
-	  base_title			# 暗黙的な返り値
-	else
-	  page_title + " | " + base_title	  # 文字列の結合
-	end
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,  "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help|Ruby on Rails Tutorial Sample App"
   end
 end
+
+
