e8fd376fe00544a01b73c73d0562bfffe22a66d9
nishikura <n-ishikura@tss-ipnet.co.jp>
2017-04-28 11:04:50 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   
   def index
-  @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])  
   end
   
   def destroy
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page:params[:page])
   end
 
   def new
