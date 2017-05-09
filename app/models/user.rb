11ad385fd0409f3f71c7025950dfe7559a55046e
nishikura <n-ishikura@tss-ipnet.co.jp>
2017-05-09 16:20:27 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+                                  
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   attr_accessor :remember_token, :activation_token, :reset_token
+  
   before_save :downcase_email
   before_create :create_activation_digest
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -13,6 +26,7 @@ class User < ActiveRecord::Base
 
 
  class << self
+  
   # 与えられた文字列のハッシュ値を返す
   def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -20,17 +34,20 @@ class User < ActiveRecord::Base
     BCrypt::Password.create(string, cost: cost)
   end
 
+
   # ランダムなトークンを返す
   def new_token
     SecureRandom.urlsafe_base64
   end
  end 
 
+
   # 永続的セッションで使用するユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
@@ -39,20 +56,24 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+
   # ユーザーログインを破棄する
   def forget
     update_attribute(:remember_digest, nil)
   end
+
   
   # アカウントを有効にする
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)  
   end
+
   
   # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
   
   # パスワード再設定の属性を設定する
   def create_reset_digest
@@ -60,26 +81,43 @@ class User < ActiveRecord::Base
     update_columns(reset_digest:  User.digest(reset_token),
                    reset_sent_at: Time.zone.now)
   end
+
   
   # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
   
-    # パスワード再設定の期限が切れている場合はtrueを返す
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
+  
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
   
-  # 試作feedの定義
-  # 完全な実装は第12章「ユーザーをフォローする」を参照してください。
+  
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
+  
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+  
+  # ユーザーをアンフォローする
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
 
  private
   # メールアドレスをすべて小文字にする
